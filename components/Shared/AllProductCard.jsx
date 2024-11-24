2ab757a521f17f62cce5a4993cc1a3f6d6d4df6c
hossain <hossainahamedk@gmail.com>
2024-11-24 14:32:31 +0600

    setup details page

--- components/Shared/AllProductCard.jsx
+++ components/Shared/AllProductCard.jsx
@@ -1,37 +1,40 @@
 import Image from "next/image";
+import Link from "next/link";
 
 const AllProductCard = ({ Product }) => {
-    const { name, image, price, rating, numberOfRating } = Product;
+    const { id, name, image, price, rating, numberOfRating } = Product;
     return (
         <div>
-            <div className="card bg-[#FFFFFF]">
-                <figure>
-                    <Image
-                        className='p-3'
-                        src={image}
-                        width={300}
-                        height={400}
-                        style={{ width: '100%', height: 'auto' }}
-                        alt='new arrival product image' />
-                </figure>
-                <div className="flex text-black space-x-2 px-4">
-                    <div className="rating">
-                        <input type="radio" name="rating-2" className="mask mask-star-2 bg-[#FFC700]" />
-                        <input type="radio" name="rating-2" className="mask mask-star-2 bg-[#FFC700]" defaultChecked />
-                        <input type="radio" name="rating-2" className="mask mask-star-2 bg-[#FFC700]" />
-                        <input type="radio" name="rating-2" className="mask mask-star-2 bg-[#FFC700]" />
-                        <input type="radio" name="rating-2" className="mask mask-star-2 bg-[#FFC700]" />
+            <Link href={`/pages/product/${id}`}>
+                <div className="card bg-[#FFFFFF] transform transition duration-500 hover:scale-105">
+                    <figure>
+                        <Image
+                            className='p-3'
+                            src={image}
+                            width={300}
+                            height={400}
+                            style={{ width: '100%', height: 'auto' }}
+                            alt='new arrival product image' />
+                    </figure>
+                    <div className="flex text-black space-x-2 px-4">
+                        <div className="rating">
+                            <input type="radio" name="rating-2" className="mask mask-star-2 bg-[#FFC700]" />
+                            <input type="radio" name="rating-2" className="mask mask-star-2 bg-[#FFC700]" defaultChecked />
+                            <input type="radio" name="rating-2" className="mask mask-star-2 bg-[#FFC700]" />
+                            <input type="radio" name="rating-2" className="mask mask-star-2 bg-[#FFC700]" />
+                            <input type="radio" name="rating-2" className="mask mask-star-2 bg-[#FFC700]" />
+                        </div>
+                        <p>({numberOfRating})</p>
                     </div>
-                    <p>({numberOfRating})</p>
+                    <div className="text-black flex p-6 justify-between">
+                        <h2 className='text-lg font-normal'>{name}</h2>
+                        <p className='text-xl font-semibold'>BDT {price}</p>
+                    </div>
+                    <div className='px-3 pb-3'>
+                        <button className='btn btn-outline w-full text-[#7E53D4] text-lg font-semibold border-2'>Add to Cart</button>
+                    </div>
                 </div>
-                <div className="text-black flex p-6 justify-between">
-                    <h2 className='text-lg font-normal'>{name}</h2>
-                    <p className='text-xl font-semibold'>BDT {price}</p>
-                </div>
-                <div className='px-3 pb-3'>
-                    <button className='btn btn-outline w-full text-[#7E53D4] text-lg font-semibold border-2'>Add to Cart</button>
-                </div>
-            </div>
+            </Link>
         </div>
     );
 };
